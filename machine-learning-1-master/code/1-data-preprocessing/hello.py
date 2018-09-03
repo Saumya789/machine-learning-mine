96e25361aacce5baceaf421d2755a7ff0d4dce33
Saumya Barnwal <saumyabarnwal789@gmail.com>
2018-09-03 21:52:31 +0530

    hello.py

---
+++ machine-learning-1-master/code/1-data-preprocessing/hello.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 21:43:55 2018
+
+@author: pradeep kr. barnwal
+"""
+
+print("Hello anaconda")
+
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
